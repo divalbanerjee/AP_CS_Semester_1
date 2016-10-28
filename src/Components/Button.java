519e4dade56ac144a9b58670724d9063dfeb82d8
Dival Banerjee <bandiv2011@gmail.com>
2016-10-28 14:58:36 -0400

    New reprository for all future ap cs projects, some files added

---
+++ src/Components/Button.java
@@ -0,0 +1,60 @@
+package Components;
+
+// This is a custom button
+//Not the same as JButton
+//Used as a custom swing component
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Button extends JPanel{
+
+    //button properties or variables
+    Font myFont = new Font("Helvetica", Font.PLAIN, 20);
+    String myButtonText = "New Button";
+    int myButtonPadding =  3;
+    int myButtonWidth = 150;
+    int myVerticalShift = 0;
+    int myButtonHeight = 60;
+    Color myPaddingColor= Color.GRAY;
+    int myPush = 0;
+    int myButtonposx = 0;
+    int mtButtonposy = 0;
+
+    public Button(Color backColor){
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        myVerticalShift = getHeight()/2  -myButtonHeight/2;
+        g.setFont(myFont);
+
+        g.setColor(new Color(144,12,63));
+        g.fillRoundRect(20+ this.myButtonPadding,myVerticalShift +this.myButtonPadding,this.myButtonWidth,this.myButtonHeight,25,25); //xpos, y pos, width, height
+
+        g.setColor(new Color(209,10,67));
+        g.fillRoundRect(20 + this.myPush,myVerticalShift + this.myPush,this.myButtonWidth,this.myButtonHeight,25,25); //xpos, y pos, width, height
+
+        g.setColor(new Color(255,195,0));
+        //g.drawString(myButtonText,(int)(getWidth()/2-myButtonText.length()*myFont.getSize()/2), (int)(getHeight()/2*myFont.getSize()/2));
+        g.drawString(this.myButtonText,-30 + this.myButtonWidth/2 , myVerticalShift+5 + this.myButtonHeight/2);
+    }
+
+    public void setFont(Font font){this.myFont = font;} //change font
+
+    public void setText(String buttonText){this.myButtonText = buttonText;} //Change text
+
+    public void setPadding(Integer paddingAmount){this.myButtonPadding = paddingAmount;} //Change text
+
+    public void setPaddingColor(Color paddingColor){this.myPaddingColor = paddingColor;} //change padding color of button
+
+    public void push(Boolean Pushed){
+        if(Pushed = true){
+            this.myPush = this.myButtonPadding;
+        }
+        else if(Pushed = false){
+            this.myPush = 0;
+        }
+    }
+}
--- src/Mains/MAINPong.java
+++ src/Mains/MAINPong.java
@@ -4,6 +4,6 @@ package Mains;
  * Created by div on 10/28/2016.
  */
 public class MAINPong {
-    //This uses the following classes:
+    //This uses the following classes: Button, game timer component, menu,
 
 }
